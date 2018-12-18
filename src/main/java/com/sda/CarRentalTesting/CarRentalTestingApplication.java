dfa602763c8b62d0efa2fef2c2378ffc462f2234
Alo <41338019+alne88@users.noreply.github.com>
2018-12-18 20:27:12 +0200

    Initial commit

---
+++ src/main/java/com/sda/CarRentalTesting/CarRentalTestingApplication.java
@@ -0,0 +1,14 @@
+package com.sda.CarRentalTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalTestingApplication.class, args);
+	}
+
+}
+
